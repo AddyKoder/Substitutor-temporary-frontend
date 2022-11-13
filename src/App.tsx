7f266e286d66738d234371268577b7688e489b94
Aditya Tripathi <productive.py@gmail.com>
2022-11-13 11:46:18 +0530

    tried fetching with localhost

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,21 @@
+import { useEffect } from 'react'
 
+async function fetchTeachers(){
+
+		const res = await fetch('http://127.0.0.1:8000/teacher/65892').catch(()=>console.log('fetching failed'))
+	
+	
+
+	if (res) {
+		const data = await res.json()
+		console.log(data);
+	}	
+
+}
 export default function App() {
+	useEffect(() => {
+		fetchTeachers()
+	},[])
+
 	return <h1> App </h1>
 }
