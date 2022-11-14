7ef7d59b582ccda4630ff0e12401e8283e4f62ce
Aditya Tripathi <productive.py@gmail.com>
2022-11-14 21:54:04 +0530

    done with create Teacher page

---
+++ src/components/TimeTableEntry.tsx
@@ -0,0 +1,85 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function TimetableEntry({ timetable, setTimetable }: { timetable: { [type: string]: string[] }; setTimetable: any }) {
+	return (
+		<div className='timetable-entry'>
+			<table>
+				<thead style={{opacity:'0.6'}}>
+					<tr>
+						<th>Day</th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+						<th>4</th>
+						<th>5</th>
+						<th>6</th>
+						<th>7</th>
+						<th>8</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					{Object.keys(timetable).map(day => {
+						return (
+							<tr key={day}>
+								<td style={{opacity:'0.6', textTransform:'uppercase'}}>{day}</td>
+								<TimetableRowEntry  day={day} row={timetable[day]} setTimetable={setTimetable} />
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function TimetableRowEntry({ row, day, setTimetable }: { row: string[]; day: string; setTimetable: any }) {
+	return (
+					<>
+			{row.map((item, index) => {
+				return (
+						<td key={`${day}-${index}`}>
+							<ClassSelector value={item}  day={day} setTimetable={setTimetable} period={index} />
+						</td>
+				);
+			})}
+					</>
+	);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function ClassSelector({value, setTimetable, day, period }: {value:string, setTimetable: any; day: string; period: number }) {
+	// creating all classes combination
+	const allClasses: string[] = [];
+	for (const classN of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]) {
+		for (const section of ['a', 'b', 'c', 'd']) {
+			allClasses.push(`${classN}-${section}`);
+		}
+	}
+
+	function updateTimetable(event:object) {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		setTimetable((timetable:any) => {
+			const e = Object(event)
+			const todayTimetable = [...timetable[day]]
+			todayTimetable[period] = e.target.value
+			const toRet = {...timetable}
+			toRet[day] = todayTimetable
+			return toRet
+		})
+	}
+	const color = value === 'free'? 'rgba(124, 124, 124, 0.6)' : 'var(--color-accent)'
+	const styles = {color:color, borderColor:color}
+	return (
+		<select name='class' value={value} onChange={updateTimetable} style={styles}>
+			<option value="free">Free</option>
+			{allClasses.map(classN => {
+				return (
+					<option key={classN} value={ classN } style={{textTransform:'uppercase'}}>
+						{classN}
+					</option>
+				);
+			})}
+		</select>
+	);
+}
--- src/pages/CreateTeacher.tsx
+++ src/pages/CreateTeacher.tsx
@@ -1,8 +1,69 @@
+import { useState } from "react"
+import TimetableEntry from "../components/TimeTableEntry"
+
+const allClasses:string[] = ['none'] 
+	for (const classN of [1,2,3,4,5,6,7,8,9,10,11,12]){
+		for (const section of ['a','b','c','d']) {
+			allClasses.push(`${classN}-${section}`)
+		}
+	}
+
 
 export default function CreateTeacher() {
+
+	const [ tName , setTname] = useState('')
+	const [ tClass, setTclass ] = useState('')
+	const [ tCategory, setTcategory] = useState('')
+	// assigning initial timetable
+	const [tTimetable, setTtimetable] = useState({
+    "mon":["free", "free", "free", "free", "free", "free", "free", "free" ],
+    "tue":["free", "free", "free", "free", "free", "free", "free", "free" ],
+    "wed":["free", "free", "free", "free", "free", "free", "free", "free" ],
+    "thu":["free", "free", "free", "free", "free", "free", "free", "free" ],
+    "fri":["free", "free", "free", "free", "free", "free", "free", "free" ],
+    "sat":["free", "free", "free", "free", "free", "free"]
+	})
+
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	function submitForm(e:any) {
+		e.preventDefault()
+	}
 	return (
 		<div className="create-teacher">
 			<h1>Add Teacher</h1>
+			<form onSubmit={submitForm} spellCheck="false">
+				{/* name input */}
+				<span>
+
+				<label htmlFor="name-input">Name</label>
+					<input type="text" name='teacher-name' id="name-input" placeholder='Name' value={tName} onChange={e => setTname(e.target.value)} />	
+				</span>
+				{/* category input */}
+				<span>
+				<label htmlFor="category-input">Category</label>
+				<select name='category' id="category-input" value={tCategory} onChange={e => setTcategory(e.target.value)}>
+					<option value='junior'>Junior</option>
+					<option value='senior'>Senior</option>
+					<option value='pgt'>PGT</option>
+				</select>
+
+</span>
+				{/* Class Teacher of Input */}
+				<span>
+				<label htmlFor="class-input">Class Teacher of</label>
+				<select name="class" id="class-input" value={tClass} onChange={e => setTclass(e.target.value)}>
+									{allClasses.map(classN => {
+										return <option key={classN} value={classN} style={{textTransform: 'uppercase'}}>{classN}</option>	
+									})}
+				</select>
+
+				</span>
+				{/* TimeTable input */}
+				<label htmlFor="timetable-entry">Time Table:</label>
+				<TimetableEntry timetable={tTimetable} setTimetable={setTtimetable} />
+				<input type="submit" className="button btn-em" style={{width: '100%'}}/>
+			</form>
 		</div>
 	) 
 	
