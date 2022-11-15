f436086c3b0ef677a3968fc2bf624cf31ac4751a
Aditya Tripathi <productive.py@gmail.com>
2022-11-15 16:46:07 +0530

    added reschedule list view

---
+++ src/components/RescheduleList.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+
+export default function RescheduleList({ reschedules }: { reschedules: object[] }) {
+	const [teachers, setTeachers] = useState<object[] | string>([]);
+	// fetching teachers
+	useEffect(() => {
+		fetch('http://127.0.0.1:8000/teacher')
+			// fetching successfully
+			.then(r => {
+				if (r.status === 200) {
+					return r.json();
+				}
+				throw new Error('invalid status code');
+			})
+			.then(r => {
+				setTeachers(r);
+			})
+			// if some error occured
+			.catch(() => {
+				setTeachers('failed');
+			});
+	}, []);
+
+	function getNameCategory(id: number):[name:string, category:string] | undefined {
+		for (const t of teachers) {
+			const teacher = Object(t);
+			if (teacher.id === id) return [teacher.name, teacher.category];
+		}
+	}
+
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>Teacher</th>
+					<th>Teacher Category</th>
+					<th>Class</th>
+					<th>Period</th>
+				</tr>
+			</thead>
+			<tbody>{reschedules.map(r => {
+				const nameCategory = getNameCategory(Object(r).teacherId)
+				if (nameCategory == undefined) return
+				return <tr key={Object(r)._id}>
+					<td>{nameCategory[0]}</td>
+					<td>{nameCategory[1]}</td>
+					<td>{Object(r).className}</td>
+					<td>{Object(r).periodNo}</td>
+				</tr>
+			})}</tbody>
+		</table>
+	);
+}
--- src/pages/Reschedules.tsx
+++ src/pages/Reschedules.tsx
@@ -1,4 +1,33 @@
+import { useEffect, useState } from 'react';
+import Spinner from '../components/Spinner';
+import Notification from '../components/Notification';
+import RescheduleList from '../components/RescheduleList';
 
 export default function Reschedules() {
-	return <h1 style={{padding: '0 .8em', margin: '.8em 0'}}>Reschedules</h1>
+	const [reschedules, setReschedules] = useState<string | object[]>('pending');
+
+	useEffect(() => {
+		fetch('http://127.0.0.1:8000/reschedule')
+			.then(r => {
+				if (r.status == 200) return r.json();
+				throw new Error('cannot fetch reschedules');
+			})
+			.then(r => {
+				if (r instanceof Array) {
+					setReschedules(r);
+				}
+			})
+			.catch(() => setReschedules('failed'));
+	}, []);
+
+
+	return (
+		<div className='reschedules'>
+			<h1 style={{ padding: '0 .8em', margin: '.8em 0' }}>Reschedules</h1>
+			{reschedules === 'pending' ? <Spinner /> :
+				reschedules === 'failed' ? <Notification type='error' heading='Cannot fetch Reschedules' content='The application cannot fetch reschedules from the database, Try restarting the application or contant the developer - Aditya Tripathi'/>:
+					<RescheduleList reschedules={reschedules as object[]} />	
+			}
+		</div>
+	);
 }
