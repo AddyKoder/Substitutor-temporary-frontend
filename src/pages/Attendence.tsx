f6894b9d835a94c6ef606fea73b30857f7178636
Aditya Tripathi <productive.py@gmail.com>
2022-11-15 10:44:22 +0530

    added logic for fetching attendence

--- src/pages/Attendence.tsx
+++ src/pages/Attendence.tsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react"
 
 export default function Attendence() {
-	const attendence = useState()
+	const [attendence, setAttendence] = useState<null | string | number[]>('fetching')
+	console.log(attendence);
+	
 
 	// fetching today's attendence
 	useEffect(() => {
-
+		fetch('http://127.0.0.1:8000/attendence').then(r => {
+			if (r.status == 200) return r.json()
+			setAttendence('error')
+			throw new Error("some error occured");
+		}).then(r => {			
+			setAttendence(r)
+		})
 	}, [])
 
 	return <div>Attendence</div>
